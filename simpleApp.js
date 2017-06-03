04db320aa370a372e3227e30aed0bb744af3560c
Hugo Avelino <hugo.avelinoh@gmail.com>
2017-06-03 12:54:19 -0500

    simple app

---
+++ simpleApp.js
@@ -0,0 +1,11 @@
+console.log('Hello world node JS')
+
+var date = new Date();
+console.log('todat is :'+date);
+
+var expFunction=function(){
+  return 'esta es una funcion en javascript';
+}
+
+console.log('resultado:'+expFunction;
+console.log('resultado:'+expFunction());
